16918369a5a0d30ac9b8b046f141a6d61f76303b
omidekz <omidekz@gmail.com>
2020-11-08 17:26:17 +0330

    make decrypt

--- fcryptor.py
+++ fcryptor.py
@@ -29,20 +29,44 @@ class FCryptor(FCryptorBase):
 	def __init__(self, key=None):
 		super().__init__(key)
 
+	@staticmethod
+	def __check_files(path, exists=True, file=True):
+		path = os.path.abspath(path)
+		if exists and not os.path.exists(path):
+			raise ValueError("{} not exists".format(path))
+		if file and not os.path.isfile(path):
+			raise ValueError("{} is not regular file".format(path))
+		return path
+	
+	@staticmethod
+	def __load_file(path):
+		return open(path, 'rb').read()
+
 	def crypt(self, inputpath, outpath=None):
-		inputpath = os.path.abspath(inputpath)
-		if not os.path.exists(inputpath):
-			raise ValueError("{} not exists".format(inputpath))
-		if not os.path.isfile(inputpath):
-			raise ValueError("{} is not regular file".format(inputpath))
-		input_file = open(inputpath, 'rb')
-		output = super().crypt(input_file.read())
-		input_file.close()
+		inputpath = FCryptor.__check_files(inputpath)
+		input_data = FCryptor.__load_file(inputpath)
+		output = super().crypt(input_data)
 
 		if outpath:
-			outpath = os.path.abspath(outpath)
+			outpath = FCryptor.__check_files(outpath, exists=False, file=False)
 			output_file = open(outpath, 'w')
 			output_file.write(output)
 			output_file.close()
 		
 		return output
+	
+	def decrypt(self, inputpath, outpath):
+		inputpath = FCryptor.__check_files(inputpath)
+		input_data = FCryptor.__load_file(inputpath)
+		output = super().decrypt(input_data)
+		
+		if outpath:
+			outpath = FCryptor.__check_files(outpath, exists=False, file=False)
+			output_file = open(outpath, 'w')
+			output_file.write(output)
+			output_file.close()
+		
+		return output
+fc = FCryptor()
+fc.crypt("./test.txt", "o.enc")
+fc.decrypt("o.enc", "oo.enc")
